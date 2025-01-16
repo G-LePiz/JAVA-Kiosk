b51a8d819370fcee6958c0d797d546a6f0d8e2e6
dawon <aodlstory321@gmail.com>
2025-01-16 10:32:05 +0900

    메뉴 추가ã

--- src/main/java/com/example/Kiosk/Main.java
+++ src/main/java/com/example/Kiosk/Main.java
@@ -12,8 +12,12 @@ public class Main {
         burgers.addmenuItems(new MenuItem("SmokeBurger", 8.9, "베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거"));
         burgers.addmenuItems(new MenuItem("CheeseBurger", 6.9, "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거"));
         burgers.addmenuItems(new MenuItem("Hambuger", 5.4, "비프패티를 기반으로 야채가 들어간 기본버거"));
-        Menu drinks = new Menu("Drinks"); // 음료라는 카테고리 추가
-        Menu desserts = new Menu("Desserts"); // 디저트라는 카테고리 추가
+        Menu drinks = new Menu("Drinks"); // Drinks라는 카테고리 추가
+        drinks.addmenuItems(new MenuItem("Americano", 4.5, "산뜻하고 신맛이 나는 원두로 만든 아메리카노"));
+        drinks.addmenuItems(new MenuItem("Ice-Americano", 5.0, "여름에 뜨거운 커피 대신에 시원한 커피를 마셔보세요!"));
+        drinks.addmenuItems(new MenuItem("CaffeLatte", 5.5, "신선한 우유와 함께 섞인 커피를 마셔보세요!"));
+        drinks.addmenuItems(new MenuItem("Cappuccino", 6.0, "토마토, 양상추, 쉑소스가 토핑된 치즈버거"));
+        Menu desserts = new Menu("Desserts"); // Desserts라는 카테고리 추가
 
         Kiosk k = new Kiosk(Arrays.asList(burgers,drinks,desserts)); // 키오스크에 카테고리를 넣음.
         try {
